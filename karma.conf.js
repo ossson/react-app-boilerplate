4b5fdea6f68f33c4f1b104bb9e082af719bb80f1
Miguel Morales <mimoralesga@gmail.com>
2018-02-09 00:10:41 -0500

    init karma conf and first test component

--- karma.conf.js
+++ karma.conf.js
@@ -1,1 +1,90 @@
+// Karma configuration
+// Generated on Thu Feb 08 2018 23:15:48 GMT-0500 (-05)
 
+module.exports = function (config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['mocha'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'test/**/*.spec.js'
+    ],
+
+
+    // list of files / patterns to exclude
+    exclude: [],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      'test/**/*.spec.js': ['webpack', 'sourcemap']
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['dots'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
+
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity,
+
+
+    //Kind of a copy of your webpack config
+    //Just do inline source maps instead of the default
+    webpack: {
+      devtool: 'inline-source-map',
+      module: {
+        loaders: [{
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        }]
+      }
+    },
+
+
+    //Please don't spam the console when running in karma!
+    webpackServer: {
+      noInfo: true
+    }
+  })
+}
