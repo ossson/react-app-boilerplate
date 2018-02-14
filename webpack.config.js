fd4cdd1ce7689be716b96db151d9e31235352ceb
Miguel Morales <mimoralesga@gmail.com>
2018-02-14 00:44:15 -0500

    add loaders rules, change path resolvers and add devServer stats

--- webpack.config.js
+++ webpack.config.js
@@ -15,15 +15,43 @@ module.exports = {
   },
   resolve: {
     modules: [
-      path.resolve('./src'),
-      path.resolve('./node_modules'),
+      path.resolve('src'),
+      path.resolve('node_modules'),
     ]
   },
   module: {
-    loaders: [
-      { test: /\.(js|jsx)$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.(js|jsx)$/, loader: 'eslint-loader', exclude: /node_modules/ }
-    ],
+    rules: [{
+        test: /\.(js|jsx)?$/,
+        use: ['babel-loader', 'eslint-loader'],
+        exclude: [/[/\\\\]node_modules[/\\\\]/]
+      },
+      {
+        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'static/media/[name].[hash].[ext]',
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /\.module\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            }
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    stats: {
+      modules: false
+    }
   },
   plugins: [htmlWebpackPluginConfig]
 }
