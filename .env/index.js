ca5b2d9396043125d3b110e412ae22f7c66c2011
Miguel Morales <mimoralesga@gmail.com>
2018-02-25 21:08:19 -0500

    se añade primerar funciones interactivas por cli

---
+++ .env/index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const vorpal = require('vorpal')()
+const shell = require('shelljs')
+
+vorpal
+  .command('start')
+  .option('--progress')
+  .action(function (args, callback) {
+    const options = [
+      args.options.progress && '--progress'
+    ]
+    shell.exec('webpack-dev-server ' + options)
+    callback()
+  })
+
+/*
+ *
+ */
+vorpal
+  .command('clear')
+  .action(function (args, cbk) {
+    let blank = ''
+    for (let i = 0; i < process.stdout.rows; ++i) {
+      blank += '\n'
+    }
+    vorpal.ui.redraw(blank)
+    vorpal.ui.redraw('')
+    cbk()
+  })
+
+/*
+ *
+ */
+vorpal
+  .delimiter('react-app~$')
+  .show()
