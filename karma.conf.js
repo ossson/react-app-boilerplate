e6fad4c44f509ad661461d0ed2068049627a40c3
Miguel Morales <mimoralesga@gmail.com>
2018-02-25 23:01:38 -0500

    change path to test components

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'test/**/*.spec.js'
+      '__tests__/**/*.spec.js'
     ],
 
 
@@ -27,7 +27,7 @@ module.exports = function (config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'test/**/*.spec.js': ['webpack', 'sourcemap']
+      '__tests__/**/*.spec.js': ['webpack', 'sourcemap']
     },
 
 
