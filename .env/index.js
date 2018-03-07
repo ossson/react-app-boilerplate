87e8cfe0973f8a6548549c720c0ada9d3328cc10
Miguel Morales <mimoralesga@gmail.com>
2018-03-07 12:24:21 -0500

    remove action serve command & add alias of clear command

--- .env/index.js
+++ .env/index.js
@@ -1,35 +1,28 @@
 'use strict';
 
 const vorpal = require('vorpal')()
-const shell = require('shelljs')
 
 /**
  *
  */
 vorpal
-  .command('start')
-  .option('--progress')
-  .action(function (args, callback) {
-    const options = [
-      args.options.progress && '--progress'
-    ]
-    shell.exec('webpack-dev-server ' + options)
-    callback()
-  })
+  .command('serve')
+  .allowUnknownOptions()
 
 /**
  *
  */
 vorpal
   .command('clear')
-  .action(function (args, callback) {
+  .alias('cls')
+  .action(function (args, cb) {
     let blank = ''
     for (let i = 0; i < process.stdout.rows; ++i) {
       blank += '\n'
     }
     vorpal.ui.redraw(blank)
     vorpal.ui.redraw('')
-    callback()
+    cb()
   })
 
 /**
