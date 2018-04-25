243c4f3ba1986df2a5e59c65e314dc0fd82a51cb
Miguel Morales <mimoralesga@gmail.com>
2018-04-24 20:32:59 -0500

    create common config

---
+++ .env/webpack.config.common.js
@@ -0,0 +1,15 @@
+import {
+  rules,
+  extensions,
+  modules
+} from './webpack'
+
+export default type => ({
+  module: {
+    rules: rules(type)
+  },
+  resolve: {
+    extensions: extensions(),
+    modules: modules()
+  }
+})
