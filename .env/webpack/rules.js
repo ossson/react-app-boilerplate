9445b22cc07a1cc6799801c4d212b455830c9372
Miguel Morales <mimoralesga@gmail.com>
2018-04-24 20:39:45 -0500

    create rules config

---
+++ .env/webpack/rules.js
@@ -0,0 +1,35 @@
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
+
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
+export default type => {
+  const rules = [{
+    test: /\.js$/,
+    use: 'babel-loader',
+    exclude: /node_modules/,
+  }]
+
+  if (!isDevelopment || type === 'server') {
+    rules.push({
+      test: /\.scss$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: [
+          'css-loader?minimize=true&modules=true&localIdentName=[name]__[local]',
+          'scss-loader'
+        ]
+      })
+    })
+  } else {
+    rules.push({
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        'css-loader?minimize=true&modules=true&localIdentName=[name]__[local]',
+        'scss-loader'
+      ]
+    })
+  }
+
+  return rules
+}
