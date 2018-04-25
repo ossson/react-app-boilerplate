874ed596af62fe744524e69d0c21ab69db7877ed
Miguel Morales <mimoralesga@gmail.com>
2018-04-24 21:36:09 -0500

    create dinamic html file

---
+++ src/server/html.js
@@ -0,0 +1,27 @@
+export default function html(options) {
+  const {
+    app = 'main',
+    title = 'react-app-boilerplate',
+    stylesheet = '/css/styles.js',
+    markup
+  } = options
+
+  return `
+    <!DOCTYPE html>
+    <html lang="es">
+      <head>
+        <meta charset="UTF-8">
+        <title>${title}</title>
+        <meta http-equiv="Content-Type" content="text/html" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <link rel="manifest" href="manifest.json">
+        <link rel="stylesheet" href="${stylesheet}">
+      </head>
+      <body>
+        <div id="root">${markup}</div>
+        <script src="/app/${app}.bundle.js"></script>
+      </body>
+    </html>
+  `
+}
