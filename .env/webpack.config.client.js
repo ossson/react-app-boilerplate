1dcbba9fff1fa9dc92bff5ab8fcd354f07be3ae6
Miguel Morales <mimoralesga@gmail.com>
2018-04-24 20:32:48 -0500

    create client config

---
+++ .env/webpack.config.client.js
@@ -0,0 +1,24 @@
+import webpackMerge from 'webpack-merge'
+
+import commonConfig from './webpack.config.common'
+import {
+  context,
+  devtool,
+  entry,
+  name,
+  output,
+  plugins,
+  target
+} from './webpack'
+
+const type = 'client'
+
+export default webpackMerge(commonConfig(type), {
+  context: context(type),
+  devtool: devtool(type),
+  entry: entry(type),
+  name: name(type),
+  output: output(type),
+  plugins: plugins(type),
+  target: target(type)
+})
