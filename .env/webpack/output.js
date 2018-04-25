a8d592685a617096a7c850ea6bb503ab1f89f403
Miguel Morales <mimoralesga@gmail.com>
2018-04-24 20:37:27 -0500

    create output config

---
+++ .env/webpack/output.js
@@ -0,0 +1,17 @@
+import path from 'path'
+
+export default type => {
+  if (type === 'server') {
+    return {
+      filename: 'server.js',
+      path: path.resolve(__dirname, '../../dist'),
+      libraryTarget: 'commonjs2'
+    }
+  }
+
+  return {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, '../../public/app'),
+    publicPath: '/app/'
+  }
+}
