9653756db508747bea3ad75bb7c0543694950259
Miguel Morales <mimoralesga@gmail.com>
2018-04-27 00:35:20 -0500

    delete functional array

--- environment/webpack/entry.js
+++ environment/webpack/entry.js
@@ -1,13 +1,4 @@
-import fs from 'fs'
-import path from 'path'
-
 const isDevelopment = process.env.NODE_ENV !== 'production'
-
-const entries = fs
-  .readdirSync(path.resolve(__dirname, '../../src/app'))
-  .filter(file => {
-    return file.match(/.*\.(js|scss)$/)
-  })
 
 export default type => {
   if (type === 'server') {
@@ -15,12 +6,14 @@ export default type => {
   }
 
   const entry = {
-    main: entries
+    main: []
   }
 
   if (isDevelopment) {
     entry.main.push('webpack-hot-middleware/client', 'react-hot-loader/patch')
   }
+
+  entry.main.push('./index.js', './styles.scss')
 
   return entry
 }
